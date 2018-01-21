d481a8bd0da7e5e030437576b57d198c9ad7d79c
stasbar <stasbar1995@gmail.com>
2018-01-21 13:24:31 +0100

    before adding ID to student
    Took 5 hours 9 minutes

--- src/main/kotlin/generator/data/Student.kt
+++ src/main/kotlin/generator/data/Student.kt
@@ -2,12 +2,11 @@ package generator.data
 
 import generator.RandomDataGenerator
 import generator.random
-import java.util.concurrent.atomic.AtomicInteger
 
 data class Student(val klass: Klass, val pesel: String, val name: String, var surname: String, val supervisorPesel : String) : Insertable {
 
-    private val id = lastId.getAndIncrement()
-    override fun toInsert() = "$id;$name;$pesel;$surname;${klass.id};$supervisorPesel"
+
+    override fun toInsert() = "$name;$pesel;$surname;${klass.id};$supervisorPesel"
 
     companion object : Schematable {
 
@@ -16,11 +15,9 @@ data class Student(val klass: Klass, val pesel: String, val name: String, var su
             return Student(klass = klass, pesel = pesel,
                     name = RandomDataGenerator.names.random(), surname = RandomDataGenerator.surnames.random(), supervisorPesel = supervisorPesel ?: pesel) //TODO boss to himself
         }
-        private val lastId = AtomicInteger(0)
         override val tableName: String = "Student"
         override val schema: String = "CREATE TABLE $tableName\n" +
                 "(\n" +
-                //"    ID INTEGER IDENTITY(1,1) NOT NULL, \n" +
                 "    Name nvarchar(40) NOT NULL,\n" +
                 "    PESEL nvarchar(11) PRIMARY KEY,\n" +
                 "    Surname nvarchar(40) NOT NULL,\n" +
